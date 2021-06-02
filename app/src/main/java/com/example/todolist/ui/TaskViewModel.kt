0514e753ab6b7f39a5f255d9f02b866b4255fc70
Eominique <slawex.on@interia.pl>
2021-06-02 15:44:39 +0200

    room search

--- app/src/main/java/com/example/todolist/ui/TaskViewModel.kt
+++ app/src/main/java/com/example/todolist/ui/TaskViewModel.kt
@@ -3,13 +3,21 @@ package com.example.todolist.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.todolist.data.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 class TaskViewModel @Inject constructor(
  private val taskDao: TaskDao
 ): ViewModel() {
 
-val tasks = taskDao.getTasks().asLiveData()
+ val searchQuery = MutableStateFlow("")
+
+ private val taskFlow = searchQuery.flatMapLatest {
+  taskDao.getTasks(it)
+ }
+
+val tasks = taskFlow.asLiveData()
 
 
 
