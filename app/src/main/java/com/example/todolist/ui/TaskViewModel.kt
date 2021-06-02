58f68873a20bc16b6d0a455afb1d56904d90fa2d
Eominique <slawex.on@interia.pl>
2021-06-02 11:53:25 +0200

    listAdapter and view binding

--- app/src/main/java/com/example/todolist/ui/TaskViewModel.kt
+++ app/src/main/java/com/example/todolist/ui/TaskViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.todolist.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.todolist.data.TaskDao
 import javax.inject.Inject
 
@@ -8,7 +9,7 @@ class TaskViewModel @Inject constructor(
  private val taskDao: TaskDao
 ): ViewModel() {
 
-
+val tasks = taskDao.getTasks().asLiveData()
 
 
 
