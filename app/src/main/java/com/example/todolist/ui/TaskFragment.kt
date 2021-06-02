17b15d12ae9af480a604931caa3fe07b36c1fbb8
Eominique <slawex.on@interia.pl>
2021-06-02 10:48:26 +0200

    room dependency

---
+++ app/src/main/java/com/example/todolist/ui/TaskFragment.kt
@@ -0,0 +1,9 @@
+package com.example.todolist.ui
+
+import androidx.fragment.app.Fragment
+import com.example.todolist.R
+
+class TaskFragment: Fragment(R.layout.fragment_tasks) {
+
+
+}
---
+++ app/src/main/java/com/example/todolist/ui/TaskViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.todolist.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.todolist.data.TaskDao
+import javax.inject.Inject
+
+class TaskViewModel @Inject constructor(
+ private val taskDao: TaskDao
+): ViewModel() {
+
+
+
+
+
+
+
+}
